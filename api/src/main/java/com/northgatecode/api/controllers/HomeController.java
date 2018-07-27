2e299dcef3e013ef070382c0e4938ba4b95853da
Leo Qian <leoq@vmware.com>
2018-07-27 16:43:58 +0800

    1

---
+++ api/src/main/java/com/northgatecode/api/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.northgatecode.api.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity index() {
+        return ResponseEntity.ok("Hello Spring Boot!");
+    }
+}
