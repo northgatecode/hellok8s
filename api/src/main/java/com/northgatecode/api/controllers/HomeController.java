a7d24464d9153e0384f05b9166adb274716e17c6
Leo Qian <leoq@vmware.com>
2018-08-03 14:53:16 +0800

    hostname

--- api/src/main/java/com/northgatecode/api/controllers/HomeController.java
+++ api/src/main/java/com/northgatecode/api/controllers/HomeController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 public class HomeController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ResponseEntity index() {
-        return ResponseEntity.ok("Hello Spring Boot!");
+    public ResponseEntity index() throws UnknownHostException {
+        return ResponseEntity.ok("Hello Spring Boot! From: " + InetAddress.getLocalHost().getHostName());
     }
 }
